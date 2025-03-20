009941c91c5248611f688cefce58afc8a0c9c02f
big-23-dev <homet5561@gmail.com>
2025-03-20 12:00:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+// Auto-redirect after 3 seconds
+window.onload = function () {
+    setTimeout(() => {
+        window.location.href = "/home";
+    }, 3000); // 2-second delay for effect
+};
+
+// Button click redirect
+document.getElementById('shopNow').addEventListener('click', () => {
+    window.location.href = "/home";
+});
 
